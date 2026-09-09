ae3afe795aae2b9773b6a02baf19456097a91416
Milos2606 <jettyho14@gmail.com>
2026-09-09 17:04:43 +0700

    fix cors error

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://cozy-florentine-bd3369.netlify.app/'],
+    'allowed_origins' => ['https://cozy-florentine-bd3369.netlify.app'],
 
     'allowed_origins_patterns' => [],
 
