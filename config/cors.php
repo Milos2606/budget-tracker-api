1acde8612b474e0455f24eb15d4e66a5b48c6732
Milos2606 <jettyho14@gmail.com>
2026-09-09 16:59:18 +0700

    update config netlify

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:5174'],
+    'allowed_origins' => ['https://cozy-florentine-bd3369.netlify.app/'],
 
     'allowed_origins_patterns' => [],
 
